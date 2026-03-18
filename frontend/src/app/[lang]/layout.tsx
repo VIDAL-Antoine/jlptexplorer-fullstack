237447b3e9c3dfa88682cd83fadaf5425c28b842
VIDAL-Antoine <avidal78390@gmail.com>
2026-03-18 22:01:20 +0100

    chore(frontend): replace mantine favicon with custom app icon

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -27,7 +27,7 @@ export default async function LocaleLayout({
     <html lang={lang} {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
